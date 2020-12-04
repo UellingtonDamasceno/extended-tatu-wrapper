d77c834a47e8ded693d5399901fe07b50bc5990f
Uellington Damasceno <uellington99@gmail.com>
2020-12-04 10:09:34 -0300

    First commit.

---
+++ src/main/java/extended/tatu/wrapper/model/ExtendedTATUMethods.java
@@ -0,0 +1,9 @@
+package extended.tatu.wrapper.model;
+
+/**
+ *
+ * @author Uellington Damasceno
+ */
+public enum ExtendedTATUMethods {
+    CONNECT, CONNACK, EVT, FLOW, GET, SET, POST, INVALID;
+}
